2f1f9373b694238f5b907ab4b59bc2881236eb33
Pedro Reis <pmreis.ual@gmail.com>
2015-11-10 23:38:50 +0000

    server alterado para retornar a mensagem que recebe

--- MotionUdpServer/Program.cs
+++ MotionUdpServer/Program.cs
@@ -28,9 +28,8 @@ namespace MotionUdpServer
                 data = serverSocket.Receive(ref udpCommsender); //A escuta no porto 1001
                 s_received = Encoding.ASCII.GetString(data, 0, data.Length); //passar os bytes para string da mensagem recebida
                 Console.WriteLine("Message received from {0}:", udpCommsender.ToString()); //imprime de onde veio a mensagem
-                Console.WriteLine(s_received); //imprime a mensagem
-                welcome = "received:"; // começa a preparar a mensagem
-                data = Encoding.ASCII.GetBytes(welcome + s_received); //Preparar mensagem de envio com um cabeçalho
+                Console.WriteLine(s_received); //imprime a mensagem               
+                //data = Encoding.ASCII.GetBytes(s_received); //Preparar mensagem de envio com um cabeçalho
                 serverSocket.Send(data, data.Length, udpCommsender); //Envio no porto 1001
 
             }
